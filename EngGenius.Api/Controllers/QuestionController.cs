4c0c6e373f6270d13cc5776c01e1261b6e79b412
Mai <mtruc7097@gmail.com>
2024-10-15 21:14:27 +0700

    add Question

---
+++ EngGenius.Api/Controllers/QuestionController.cs
@@ -0,0 +1,25 @@
+﻿using EngGenius.Api.DTO;
+using EngGenius.Database;
+using EngGenius.Domains.Enum;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EngGenius.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public QuestionController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("CreateQuestions")]
+        public async Task<ActionResult<QuestionResponseDTO>> CreateQuestions(EnumLevel level, int userId, int totalQuestions)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ EngGenius.Api/DTO/QuestionResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace EngGenius.Api.DTO
+{
+    public class QuestionResponseDTO
+    {
+        public string Content {  get; set; }
+        public List<string> Choices { get; set; }
+        public int ResultChoiceIndex { get; set; }
+    }
+}
